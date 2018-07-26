4008a427670b37993a4c79f2c419d64e323877f4
Crisley Alves <crisley.moura@compasso.com.br>
2018-07-26 19:42:09 -0300

    Tratamento do CORS implementado - OK

--- app.js
+++ app.js
@@ -12,6 +12,18 @@ app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+app.use((req, res, next)=>{
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers", 
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    if(req.method === "OPTIONS"){
+        res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+        return res.status(200).json({});
+    }
+    next();
+})
+
 //para a rota X, utilize as informações desta variável
 app.use('/products', productRoutes);
 app.use('/order', orderRoutes);
